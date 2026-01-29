5e0515cd758ef9cd3910844272fe426557de6a0e
Sujal Mehta <sujalmehta@Sujals-MacBook-Air.local>
2026-01-29 13:02:36 +0545

    Template Inheritance Variable Passing and Using that

--- djangoBasics/djangoBasics/initial_test.py
+++ djangoBasics/djangoBasics/initial_test.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request , "index.html")
+    return render(request , "index.html" , {"user_name" : "Sujal Mehta"})
